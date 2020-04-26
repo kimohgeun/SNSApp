c3d74286d1814cdbe09155af7f4e957940a143e8
kimohgeun <ohgoodkim@gmail.com>
2020-04-26 21:44:58 +0900

    PasswordReset 컴포넌트 생성

--- src/Screens/Navigator.tsx
+++ src/Screens/Navigator.tsx
@@ -3,9 +3,11 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import CheckLogin from '~/Screens/CheckLogin';
 import Login from '~/Screens/Login';
+import PasswordReset from '~/Screens/PasswordReset';
 
 const LoginNavigator = createStackNavigator({
   Login,
+  PasswordReset,
 });
 
 const AppNavigator = createSwitchNavigator(
